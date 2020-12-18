1f140598e7d271c869ea8f7a1e204c39c2d47331
Craig White <craigwhiteofficial@gmail.com>
2020-12-18 17:46:55 -0500

    Wrapped _setUserState in try-catch in handleRedirectCallback
    - This will ensure that the user will be sent back to the login screen in the event and error occurs

--- src/auth/Auth0.js
+++ src/auth/Auth0.js
@@ -62,10 +62,16 @@ export const Auth0 = () => {
             },
             /** Handles the callback when logging in using a redirect */
             async handleRedirectCallback(url) {
-                await this.auth0Client.handleRedirectCallback(url);
-                this.isAuthenticated = await this.auth0Client.isAuthenticated();
-                if (this.isAuthenticated) {
-                    await this._setUserState();
+                try {
+                    await this.auth0Client.handleRedirectCallback(url);
+                    this.isAuthenticated = await this.auth0Client.isAuthenticated();
+                    if (this.isAuthenticated) {
+                        await this._setUserState();
+                    }
+                } catch (error) {
+                    this.logout()
+                } finally {
+                    this.loading = false;
                 }
             },
             async getTokenSilently() {
